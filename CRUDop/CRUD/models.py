834009e98170e70fc32d1b88388f6d3fa3d897a1
Titas Das <dastitas21@gmail.com>
2021-05-02 21:52:26 +0530

    final updation with bool

--- CRUDop/CRUD/models.py
+++ CRUDop/CRUD/models.py
@@ -1,12 +1,15 @@
 from django.db import models
+from django.contrib.sites.models import Site
 
 class billmodel(models.Model):
     c_id=models.IntegerField(primary_key=True)
     c_name=models.CharField(max_length=150)
     email=models.CharField(max_length=100)
+    job=models.CharField(max_length=9)
     gender=models.CharField(max_length=1)
-    watt_used=models.IntegerField()
-    
+    watt_used=models.FloatField()
+    payment = models.BooleanField()
+    site = models.ForeignKey(Site, blank=True, null=True, on_delete=models.CASCADE)
     class Meta:
-        db_table= "billModel"
+        db_table= "billtable"
     
--- CRUDop/CRUD/views.py
+++ CRUDop/CRUD/views.py
@@ -1,45 +1,68 @@
 from django.shortcuts import render
 from django.contrib import messages
 from .models import billmodel
-from CRUD.forms import forms
+from .forms import cusforms
+from django.http import Http404
+
 
 def showcustomer(request):
-    showall=billmodel.objects.all()
-    return render(request,'index.html',{"data":showall})
+    showall = billmodel.objects.all()
+    return render(request, 'index.html', {"data": showall})
+
 
 def insertcustomer(request):
-    if request.method=="POST":
-        if request.POST.get('c_id') and request.POST.get('c_name') and request.POST.get('email') and request.POST.get('gender') and request.POST.get('watt_used'):
-            saverecord=billmodel()
-            saverecord.c_id=request.POST.get('c_id')
-            saverecord.c_name=request.POST.get('c_name')
-            saverecord.email=request.POST.get('email')
-            saverecord.gender=request.POST.get('gender')
-            saverecord.watt_used=request.POST.get('watt_used')
+    if request.method == "POST":
+        if request.POST.get('c_id') and request.POST.get('c_name') and request.POST.get('email') and request.POST.get('job') and request.POST.get('gender') and request.POST.get('watt_used') and request.POST.get('payment'):
+            saverecord = billmodel()
+            saverecord.c_id = request.POST.get('c_id')
+            saverecord.c_name = request.POST.get('c_name')
+            saverecord.email = request.POST.get('email')
+            saverecord.job = request.POST.get('job')
+            saverecord.gender = request.POST.get('gender')
+            saverecord.watt_used = request.POST.get('watt_used')
+            saverecord.payment = request.POST.get('payment')
             saverecord.save()
-            messages.success(request,'Customer '+saverecord.c_name+' is saved sucessfully')
-            return render(request,'insert.html')
-    
+            messages.success(request, 'Customer ' +
+                             saverecord.c_name+' is saved sucessfully')
+            return render(request, 'insert.html')
+
     else:
-        return render(request,'insert.html')
-    
-    
+        return render(request, 'insert.html')
+
+
+# def editcus(request, c_id):
+#     editcusobj = billmodel.objects.get(c_id=c_id)
+#     return render(request, 'edit.html', {"billmodel": editcusobj})
+
+
+# def updatecus(request, c_id):
+#     Updatecus = billmodel.objects.get(c_id=c_id)
+#     form = cusforms(request.POST, instance=Updatecus)
+#     if form.is_valid():
+#         form.save()
+#         messages.success(request, 'request updated')
+#         return render(request, 'edit.html', {"billmodel": Updatecus})
+#     else:
+#         raise Http404
 
 def editcus(request, c_id):
-    editcusobj=billmodel.objects.get(c_id=c_id)
-    return render(request, 'edit.html',{"billmodel":editcusobj})
+    editcusobj = billmodel.objects.get(c_id=c_id)
+    return render(request, 'edit.html', {"billmodel": editcusobj})
+
 
 def updatecus(request,c_id):
-    Updatecus=billmodel.objects.get(c_id=c_id)
-    form=cusforms(request.POST,instance=Updatecus)
+    Updatecus = billmodel.objects.get(c_id=c_id)
+    form = cusforms(request.POST, instance=Updatecus)
     if form.is_valid():
         form.save()
-        messages.success(request, "request updated")
-        return render(request, 'edit.html',{"billmodel":Updatecus})
+        messages.success(request, 'Record Updated')
+        return render(request, 'edit.html', {"billmodel": Updatecus})
+    else:
+        editcus(request, c_id)
 
-
-def delcus(request,c_id):
-    delcus=billmodel.objects.get(c_id=c_id)
+def delcus(request, c_id):
+    delcus = billmodel.objects.get(c_id=c_id)
     delcus.delete()
-    showdata=billmodel.objects.all()
-    return render(request, "index.html",{"billmodel":showdata})
+    showdata = billmodel.objects.all()
+    print(showdata)
+    return render(request, 'index.html', {"data": showdata})
